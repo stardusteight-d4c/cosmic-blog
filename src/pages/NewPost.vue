4abd4b71bf7f9fb98e1ad5e49ca88cb4f3cf9822
Gabriel Sena <stardusteight.d4cc@gmail.com>
2023-02-27 22:04:39 -0300

    rich text editor

--- src/pages/NewPost.vue
+++ src/pages/NewPost.vue
@@ -1,11 +1,9 @@
 <script lang="ts">
-import { defineComponent, ref, watchEffect } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'NewPost',
   setup() {
-    const text = ref()
-    const textWrapper = ref([])
     const iconStyle = {
       size: 42,
       color: '#F2F2F2',
@@ -13,23 +11,62 @@ export default defineComponent({
       class: `p-2 hover:bg-white/20 transition-all duration-300 ease-in-out rounded-lg cursor-pointer`,
     }
 
-    watchEffect(() => {
-      console.log(text.value)
-    })
+    const textContent = ref('')
+    const oldValue = ref('')
+    const textarea = ref<HTMLTextAreaElement>()
 
-    const handleSelected = (selected: string) => {
-      if (selected === 'bold') {
-        const applyBold = '<strong>' + text.value + '</strong>'
+    const handleSelected = (type: string) => {
+      const textareaElement = textarea.value
+      if (!textareaElement) {
+        return
       }
-      // { bold: 'Fala pessoal!', default: 'Tudo bem com vocês?' }
-      // ['<strong>Fala pessoal!</strong>', 'Tudo bem com vocês?']
+      const selectionStart = textareaElement.selectionStart
+      const selectionEnd = textareaElement.selectionEnd
+      console.log('textarea.value?.textLength', textarea.value?.textLength);
+      
+      console.log('selectionStart', selectionStart, 'selectionEnd', selectionEnd);
+      
+      const selectedText = textareaElement.value.slice(
+        selectionStart,
+        selectionEnd
+      )
+      const newValue =
+        textareaElement.value.slice(0, selectionStart) +
+        getFormattedText(selectedText, type) +
+        textareaElement.value.slice(selectionEnd)
+
+      textContent.value = newValue
+      oldValue.value = textareaElement.value
+    }
+
+    const getFormattedText = (text: string, type: string) => {
+      switch (type) {
+        case 'bold':
+          return `**${text}**`
+        case 'italic':
+          return `_${text}_`
+        case 'underline':
+          return `<u>${text}</u>`
+        case 'link':
+          return `<a href="insira o link">${text}</a>`
+        case 'image':
+          return `<img src="${text}" alt="">`
+        case 'code':
+          return `\`\`\`${text}\`\`\``
+        case 'align-left':
+          return `<div style="text-align: left;">${text}</div>`
+        case 'align-center':
+          return `<div style="text-align: center;">${text}</div>`
+        case 'align-right':
+          return `<div style="text-align: right;">${text}</div>`
+      }
     }
 
     return {
       iconStyle,
+      textContent,
       handleSelected,
-      text,
-      textWrapper,
+      textarea,
     }
   },
 })
@@ -38,7 +75,9 @@ export default defineComponent({
 <template>
   <div class="bg-[#171717] min-h-screen w-screen">
     <div class="max-w-7xl mx-auto">
-      <div class="border mx-auto w-full rounded-2xl overflow-hidden max-w-[1000px] h-fit">
+      <div
+        class="border mx-auto w-full rounded-2xl overflow-hidden max-w-[1000px] h-fit"
+      >
         <div
           class="bg-[#111111] text-[#F2F2F2] flex gap-2 px-4 py-2 rounded-t-lg"
         >
@@ -46,10 +85,16 @@ export default defineComponent({
             v-on:click="handleSelected('bold')"
             v-bind="iconStyle"
           />
-          <ph-text-italic v-on:click="handleSelected" v-bind="iconStyle" />
-          <ph-text-underline v-bind="iconStyle" />
-          <ph-link v-on:click="handleSelected" v-bind="iconStyle" />
-          <ph-image v-on:click="handleSelected" v-bind="iconStyle" />
+          <ph-text-italic
+            v-on:click="handleSelected('italic')"
+            v-bind="iconStyle"
+          />
+          <ph-text-underline
+            v-on:click="handleSelected('underline')"
+            v-bind="iconStyle"
+          />
+          <ph-link v-on:click="handleSelected('link')" v-bind="iconStyle" />
+          <ph-image v-on:click="handleSelected('image')" v-bind="iconStyle" />
           <ph-code v-on:click="handleSelected" v-bind="iconStyle" />
 
           <ph-text-align-left v-on:click="handleSelected" v-bind="iconStyle" />
@@ -64,7 +109,8 @@ export default defineComponent({
           class="bg-[#111111] text-[#F2F2F2] border-t border-t-white/10 flex gap-2 w-full max-w-[1000px] mx-auto rounded-b-lg"
         >
           <textarea
-            v-model="text"
+            ref="textarea"
+            v-model="textContent"
             class="bg-[#111111] p-4 outline-none text-[#F2F2F2] w-full h-full"
           />
         </div>
