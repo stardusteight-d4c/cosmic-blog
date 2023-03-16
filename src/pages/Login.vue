2bb92bc7816c4e047a3436cd658c861a3b66d870
Gabriel Sena <stardusteight.d4cc@gmail.com>
2023-03-16 20:30:18 -0300

    login page form

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -3,10 +3,11 @@ import { defineComponent, ref, computed } from 'vue'
 import Navbar from '@/components/navbar/Navbar.vue'
 import ChooseAvatarPopUp from '@/components/ChooseAvatarPopUp.vue'
 import { chooseAvatars } from '@/utils/mock-data'
+import ArrowRight from '@/atoms/icons/ArrowRight.vue'
 
 export default defineComponent({
   name: 'Login',
-  components: { Navbar, ChooseAvatarPopUp },
+  components: { Navbar, ChooseAvatarPopUp, ArrowRight },
   setup() {
     const proceedToChooseAvatar = ref(false)
     const selectedAvatar = ref<null | string>(null)
@@ -42,13 +43,20 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="bg-[#1a1a1a] text-[#F2F2F2] min-h-screen w-screen">
+  <div
+    class="bg-[#1a1a1a] text-[#F2F2F2] h-full max-h-screen w-screen overflow-hidden"
+  >
     <portal-target name="chooseAvatarPopUp"></portal-target>
-    <div class="max-w-[725px] h-full w-full mx-auto mb-8">
+    <div class="max-w-[725px] relative min-h-screen w-full mx-auto">
       <Navbar path="login" />
-      <div class="shadowww w-full mx-auto py-8 px-14 rounded-sm">
-        <div>
-          <h1 class="text-4xl font-bold mb-8">Sign Up</h1>
+      <div
+        class="w-full absolute top-1/2 -translate-y-1/2 h-fit mx-auto px-28 rounded-sm"
+      >
+        <div class="text-center mb-8">
+          <h1 class="text-4xl font-bold">Sign Up</h1>
+          <span class="text-center inline-block text-lg text-[#F2F2F2]/70 mt-2"
+            >Already have an account? <span class="text-blue-500 hover:underline cursor-pointer">Sign In</span>
+          </span>
         </div>
         <div class="flex items-center gap-x-4">
           <div class="form-control">
@@ -111,44 +119,48 @@ export default defineComponent({
             </label>
           </div>
         </div>
-        <div class="mt-4">
-          <h2 class="text-2xl font-bold text-center mb-8">
-            Choose your avatar!
-          </h2>
-          <div class="w-full flex items-center justify-center h-32 relative">
-            <div
-              class="w-32 h-32 rounded-full hover:scale-105 ease-in-out active:scale-95 transition-all duration-150 relative cursor-pointer flex items-center justify-center"
-            >
-              <img
-                v-if="selectedAvatar === null"
-                @click="proceedToChooseAvatar = true"
-                src="../assets/avatar-placeholder.svg"
-                class="inner-shadow w-32 bg-[#1A1A1A] absolute z-[100] rounded-full object-cover"
-              />
-              <ChooseAvatarPopUp
-                @selectedChooseAvatarPopUp="selectedChooseAvatarPopUpObserver"
-                @closedChooseAvatarPopUp="closedChooseAvatarObserver"
-                v-if="proceedToChooseAvatar"
-              />
+        <!-- <div class="mt-4">
+            <h2 class="text-2xl text-center font-bold mb-8">
+              Choose your avatar!
+            </h2>
+            <div class="w-full flex items-center justify-center h-32 relative">
               <div
-                @click="proceedToChooseAvatar = true"
-                v-if="selectedAvatar"
-                class="absolute inset-0 flex items-center justify-center"
+                class="w-32 h-32 rounded-full relative cursor-pointer flex items-center justify-center"
               >
                 <img
-                  :src="avatarUrl!"
-                  class="pendulum overflow-visible absolute pointer-events-none w-36 z-[100] mx-auto"
+                  v-if="selectedAvatar === null"
+                  @click="proceedToChooseAvatar = true"
+                  src="../assets/avatar-placeholder.svg"
+                  class="inner-shadow w-32 bg-[#1A1A1A] transition-all duration-500 hover:scale-110 ease-in-out active:scale-95 absolute z-[100] rounded-full object-cover"
+                />
+                <ChooseAvatarPopUp
+                  @selectedChooseAvatarPopUp="selectedChooseAvatarPopUpObserver"
+                  @closedChooseAvatarPopUp="closedChooseAvatarObserver"
+                  v-if="proceedToChooseAvatar"
                 />
                 <div
-                  class="box-animate w-32 h-32 absolute z-10 bg-gradient-to-t from-blue-500 to-violet-500 rounded-full mx-auto"
-                />
-                <div
-                  class="inner-shadow w-[120px] h-[120px] pointer-events-none absolute z-50 bg-[#1a1a1a] rounded-full mx-auto"
-                />
+                  @click="proceedToChooseAvatar = true"
+                  v-if="selectedAvatar"
+                  class="absolute inset-0 flex items-center justify-center"
+                >
+                  <img
+                    :src="avatarUrl!"
+                    class="pendulum overflow-visible absolute pointer-events-none w-64 z-[100] mx-auto"
+                  />
+                  <div
+                    class="box-animate w-32 h-32 absolute z-10 bg-gradient-to-t from-blue-500 to-violet-500 rounded-full mx-auto"
+                  />
+                  <div
+                    class="inner-shadow w-[120px] h-[120px] pointer-events-none absolute z-50 bg-[#1a1a1a] rounded-full mx-auto"
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          </div> -->
+        <button class="w-fit text-xl mx-auto mt-8 font-semibold">
+          <ArrowRight />
+          Next
+        </button>
       </div>
     </div>
   </div>
@@ -235,5 +247,27 @@ export default defineComponent({
   100% {
     transform: rotate(5deg);
   }
+}
+
+button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  background-image: linear-gradient(to top, #3b82f6, #8b5cf6);
+  background-clip: padding-box;
+  color: #f2f2f2;
+  padding: 4px 18px;
+  border-radius: 999px;
+  transition: all 0.5s ease;
+}
+
+button:active {
+  transform: scale(.9) !important;
+  transition: all 100ms ease;
+}
+
+button svg {
+  width: 16px;
 }
 </style>
